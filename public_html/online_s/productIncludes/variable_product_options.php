8b78f9f1b647ac0385631e7d204ca7d07f214f9a
Farah Shaheen <farah.ma.shaheen@gmail.com>
2023-03-24 16:09:03 +0300

    fixed spacing

--- public_html/online_s/productIncludes/variable_product_options.php
+++ public_html/online_s/productIncludes/variable_product_options.php
@@ -1,28 +1,20 @@
 <?php 
-
 if($pro_type == "variable_product"){
-
 $select_product_variations = "select * from product_variations where product_id='$pro_id' and not product_type='default_attributes_variation'";
 
 $run_product_variations = mysqli_query($con,$select_product_variations);
-
 $count_product_variations = mysqli_num_rows($run_product_variations);
 
 if($count_product_variations != 0){
-	
 $select_default_variation = "select * from product_variations where product_id='$pro_id' and product_type='default_attributes_variation'";
-
 $run_default_variation = mysqli_query($con,$select_default_variation);
-
 $count_default_variation = mysqli_num_rows($run_default_variation);
-
 if($count_default_variation != 0){
-
 $row_default_variation = mysqli_fetch_array($run_default_variation);
-
 $default_variation_id = $row_default_variation["variation_id"];
+}
 
-}else{
+else{
 
 $default_variation_id = 0;	
 	
@@ -63,23 +55,15 @@ $default_variation_meta_array[$i]["meta_value"] = "meta_value='$meta_value'";
 }
 
 $product_variations = array();
-
 $select_product_variations = "select * from product_variations where product_id='$pro_id' and not product_type='default_attributes_variation'";
-
 $run_product_variations = mysqli_query($con,$select_product_variations);
-
 while($row_product_variations = mysqli_fetch_array($run_product_variations)){
-	
-$variation_id = $row_product_variations["variation_id"];
-
+	$variation_id = $row_product_variations["variation_id"];
 array_push($product_variations, $variation_id);
-
 }
 
 $variation_ids = implode(",", $product_variations);
-
 $attribute_i = 0;
-
 ?>
 
 <div id="variable-product-div"><!-- variable-product-div Starts -->
@@ -87,33 +71,20 @@ $attribute_i = 0;
 <?php 
 
 $select_product_attributes = "select * from product_attributes where product_id='$pro_id'";
-
 $run_product_attributes = mysqli_query($con,$select_product_attributes);
-
 while($row_product_attributes = mysqli_fetch_array($run_product_attributes)){
-	
 $attribute_i++;
-	
 $attribute_name = $row_product_attributes["attribute_name"];
-
 $meta_key = str_replace(' ', '_', strtolower($attribute_name));
-
 $select_default_variation_meta = "select * from variations_meta where variation_id='$default_variation_id' and meta_key='$meta_key'";
-
 $run_default_variation_meta = mysqli_query($con,$select_default_variation_meta);
-
 $row_default_variation_meta = mysqli_fetch_array($run_default_variation_meta);
-
 $default_meta_value = $row_default_variation_meta["meta_value"];
-
 $attribute_variation_meta_array = $default_variation_meta_array;
 
 if(!empty($default_meta_value)){
-
 unset($attribute_variation_meta_array[$attribute_i]);
-
 array_unshift($attribute_variation_meta_array,"");
-
 unset($attribute_variation_meta_array[0]);
 
 }
@@ -121,14 +92,12 @@ unset($attribute_variation_meta_array[0]);
 
 
 if(!empty($attribute_variation_meta_array)){
-	
-$variation_meta_variation_ids = array();
-
+	$variation_meta_variation_ids = array();
 $loop_number = 0;
 
-foreach($attribute_variation_meta_array as $array_id => $variation_meta){
+    foreach($attribute_variation_meta_array as $array_id => $variation_meta){
 		
-$loop_number++;
+    $loop_number++;
 		
 $variation_meta = implode(" and ", $variation_meta);
 
